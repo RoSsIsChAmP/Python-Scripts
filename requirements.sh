7b1acbf7930c26b01cac53c176608986291f7027
Mitchell Blystone-Pemberton <mpemberton8@gmail.com>
2020-01-24 11:53:42 -0700

    Update requirements.sh
    updated the whois package name

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #This file is used to install dependencies for the use of the Website Lookup tool written by Mitchell Pemberton. 
 
-sudo pip install xlrd imgkit datetime whois && sudo apt-get install wkhtmltopdf -y 
+sudo pip install xlrd imgkit datetime python-whois && sudo apt-get install wkhtmltopdf -y 
 
 
 
