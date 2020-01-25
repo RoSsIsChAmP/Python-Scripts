06bfeab8b7049136991325798d675fc521afd78f
Mitchell Blystone-Pemberton <mpemberton8@gmail.com>
2020-01-24 21:15:44 -0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
 #This file is used to install dependencies for the use of the Website Lookup tool written by Mitchell Pemberton. 
 
+sudo dpkg --configure -a
+
 sudo pip install xlrd imgkit datetime python-whois && sudo apt-get install wkhtmltopdf -y 
 
 
